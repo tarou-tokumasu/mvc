549e9d32a90fdac1dfc781a71292150298bc80fc
tarou-tokumasu <44559640+tarou-tokumasu@users.noreply.github.com>
2019-04-08 04:18:02 +0900

    0408

--- test/test/Controllers/HomeController.cs
+++ test/test/Controllers/HomeController.cs
@@ -8,18 +8,29 @@ namespace test.Controllers
 {
     public class HomeController : Controller
     {
+
+        // "Controller"を排除した local/Homeに関するコントローラー
+        //ActionResultはjavaのget
         public ActionResult Index()
         {
+            //どれ？ Viewsフォルダの対応したcshtml?
+            //その様子　同名ファイルでないとエラー
             return View();
         }
 
-        public ActionResult About()
+        // home/About/
+        //_Layout.cshtmlで用意したパラメータ受け取るのはここで
+        public ActionResult About(int test)
         {
+            //cshtmlで使うための変数用意　cshtmlの方でもできる
+            ViewBag.age = test;
+            ViewBag.name = "田中";
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
+        // home/Contact
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
